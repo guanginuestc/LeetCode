1f53f1f5fb9727a7b697cffe7b7b26eb6ce82624
guanginuestc <h_n_sjg@163.com>
2018-04-30 14:34:48 +0800

    70.动态规划

---
+++ LeetCode819.cpp
@@ -0,0 +1,48 @@
+map<string, int> split(string inputstring, string c) {
+    vector<string> result;
+    map<string, int> temp_map;
+    string::size_type pos1 = 0, pos2 = 0;
+    string temp;
+    while (pos2 < inputstring.size()) {
+        pos2 = inputstring.find(c, pos2);
+        if (pos2 == -1) {
+            while (pos1 <inputstring.size()) {
+                if (isalpha(inputstring[pos1]))
+                    temp += tolower(inputstring[pos1]);
+                pos1++;
+            }
+            if (temp.size())
+                temp_map[temp] ++;
+            break;
+        }
+        while (pos1 <pos2) {
+            if (isalpha(inputstring[pos1]))
+                temp += tolower(inputstring[pos1]);
+            pos1++;
+        }
+        pos2 += c.size();
+        if (temp.size())
+            temp_map[temp] ++;
+        result.push_back(temp);
+        temp = "";
+        pos1 = pos2;
+    }
+    return temp_map;
+}
+string mostCommonWord(string paragraph, vector<string>& banned) {
+    map<string, int> ss = split(paragraph, " ");
+    for (vector<string>::iterator i = banned.begin(); i != banned.end(); i++) {
+        ss[*i] = 0;
+    }
+    string temp;
+    int temp_num = 0;
+    for (map<string, int>::iterator ix = ss.begin(); ix != ss.end(); ix++) {
+
+        if (temp_num< ix->second) {
+            temp_num = ix->second;
+            temp = ix->first;
+        }
+    }
+    return temp;
+
+}
